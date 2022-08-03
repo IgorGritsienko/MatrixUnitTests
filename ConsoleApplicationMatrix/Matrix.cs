6450cff498906c28aea108cfe4f953e7b2d2fc73
i.gritsienko <talen1298@mail.ru>
2022-08-03 14:53:48 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplicationMatrix/Matrix.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationMatrix
+{
+    public class MyException : Exception { public MyException(string s) : base(s) { } }
+    public class Matrix
+    {
+        int[,] m;
+        //Свойство для работы с числом строк.
+        public int J { get; set; }
+
+        //Свойство для работы с числом столбцов.
+        public int I { get; set; }
+
+        //Конструктор.
+        public Matrix(int i, int j)
+        {
+            if (i <= 0) throw new 
+                    MyException(string.Format("недопустимое значение i = {0}", i));
+            if (j <= 0) throw new
+                    MyException(string.Format("недопустимое значение j = {0}", j));
+            I = i;
+            J = j;
+            m = new int[i, j];
+        }
+        
+        //Индексатор для доступа к значениям компонентов матрицы.
+        public int this[int i, int j]
+        {
+            get
+            {
+                if (i < 0 | i > I - 1) throw new
+                        MyException(string.Format("неверное значение i = {0}", i));
+                if (j < 0 | j > J - 1) throw new
+                        MyException(string.Format("неверное значение j = {0}", j));
+                return m[i, j];
+            }
+            set
+            {
+                if (i < 0 | i > I - 1) throw new
+                        MyException(string.Format("неверное значение i = {0}", i));
+                if (j < 0 | j > J - 1) throw new
+                        MyException(string.Format("неверное значение j = {0}", j));
+                m[i, j] = value;
+            }
+        }
+
+        public static Matrix operator +(Matrix a, Matrix b) 
+        {
+            if (a.I != b.I || a.J != b.J) throw new 
+                    MyException(string.Format("неверные размерности матрицы"));
+
+            Matrix c = new Matrix(a.I, a.J);
+            for (int i = 0; i < a.I; i++) 
+                for (int j = 0; j < a.J; j++)
+                {
+                    c[i, j] = a.m[i, j] + b.m[i, j]; 
+                }
+            return c;
+        }
+
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            if (a.I != b.I || a.J != b.J) throw new
+                    MyException(string.Format("Неверные размерности матрицы"));
+
+            Matrix c = new Matrix(a.I, a.J);
+            for (int i = 0; i < a.I; i++)
+                for (int j = 0; j < a.J; j++)
+                {
+                    c[i, j] = a.m[i, j] - b.m[i, j];
+                }
+            return c;
+        }
+
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            if (a.J != b.I) throw new
+                    MyException(string.Format("Неверные размерности матриц"));
+
+            Matrix c = new Matrix(a.I, a.J);
+            for (int i = 0; i < a.I; i++)
+                for (int j = 0; j < a.J; j++)
+                {
+                    c[i, j] = 0;
+                    for (int k = 0; k < b.J; k++)
+                        c[i, j] += a.m[i, k] * b.m[k, j];
+                }
+            return c;
+        }
+
+        public static bool operator ==(Matrix a, Matrix b) 
+        {
+            bool q = true; 
+            for (int i = 0; i < a.I; i++) 
+                for (int j = 0; j < a.J; j++) 
+                {
+                    if (a[i, j] != b[i, j])
+                    {
+                        q = false; break; 
+                    }
+                } 
+            return q;
+        }
+
+        public static bool operator !=(Matrix a, Matrix b)
+        {
+            return !(a == b);
+        }
+
+        public static Matrix Transp(Matrix a)
+        {
+            Matrix c = new Matrix(a.I, a.J);
+            if (a.I != a.J) throw new
+                   MyException(string.Format("матрица для транспонирования должна быть квадратной."));
+            for (int i = 0; i < a.I; i++)
+                for (int j = 0; j < a.J; j++)
+                    c[j, i] = a.m[i, j];
+            return c;
+        }
+
+        public static int Min(Matrix a)
+        {
+            int min = a.m[0, 0];
+            for (int i = 0; i < a.I; i++)
+                for (int j = 0; j < a.J; j++)
+                    if (a.m[i, j] < min)
+                        min = a.m[i, j];
+            return min;
+        }
+
+        public static string[,] ToString(Matrix a)
+        {
+            string[,] str = new string[a.I, a.J];
+            for (int i = 0; i < a.I; i++)
+                for (int j = 0; j < a.J; j++)
+                    str[i, j] = a[i, j].ToString();
+            return str;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (this as Matrix) == (obj as Matrix);
+        }
+    }
+}
---
+++ MatrixTests/MatrixTests.cs
@@ -0,0 +1,196 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApplicationMatrix;
+
+namespace MatrixTests
+{
+    [TestClass]
+    public class MatrixTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(MyException))]
+        public void Matrix_Expected_MyException_i()
+        {
+            Matrix a = new Matrix(0, 2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyException))]
+        public void Matrix_Expected_MyException_j()
+        {
+            Matrix a = new Matrix(2, -1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyException))]
+        public void Matrix_Expected_MyException_get_i()
+        {
+            Matrix a = new Matrix(2, 2);
+            int r = a[3, 1];
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyException))]
+        public void Matrix_Expected_MyException_get_j()
+        {
+            Matrix a = new Matrix(2, 2);
+            int r = a[1, 3];
+        }
+
+       [TestMethod]
+        public void Matrix_Equal()
+        {
+            Matrix a = new Matrix(2, 2);
+            a[0, 0] = 1; a[0, 1] = 1; a[1, 0] = 1; a[1, 1] = 1;
+            Matrix b = new Matrix(2, 2);
+            b[0, 0] = 1; b[0, 1] = 1; b[1, 0] = 1; b[1, 1] = 1;
+            bool r = a == b;
+            Assert.IsTrue(r);
+        }
+
+        [TestMethod]
+        public void Matrix_Sum()
+        {
+            Matrix a = new Matrix(2, 2);
+            a[0, 0] = 1; a[0, 1] = 1; a[1, 0] = 1; a[1, 1] = 1;
+            Matrix b = new Matrix(2, 2);
+            b[0, 0] = 2; b[0, 1] = 2; b[1, 0] = 2; b[1, 1] = 2;
+
+            Matrix expected = new Matrix(2, 2);
+            expected[0, 0] = 3; expected[0, 1] = 3;
+            expected[1, 0] = 3; expected[1, 1] = 3;
+
+            Matrix actual = new Matrix(2, 2);
+            actual = a + b;
+
+            Assert.IsTrue(actual == expected);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyException))]
+        public void Matrix_Expected_MyException_Sum()
+        {
+            Matrix a = new Matrix(1, 2);
+            a[0, 0] = 1; a[0, 1] = 1;
+            Matrix b = new Matrix(2, 2);
+            b[0, 0] = 2; b[0, 1] = 2; b[1, 0] = 2; b[1, 1] = 2;
+            Matrix res = a + b;
+        }
+
+        [TestMethod]
+        public void Matrix_Sub()
+        {
+            Matrix a = new Matrix(2, 2);
+            a[0, 0] = 1; a[0, 1] = 1; a[1, 0] = 1; a[1, 1] = 1;
+            Matrix b = new Matrix(2, 2);
+            b[0, 0] = 2; b[0, 1] = 2; b[1, 0] = 2; b[1, 1] = 2;
+
+            Matrix expected = new Matrix(2, 2);
+            expected[0, 0] = -1; expected[0, 1] = -1;
+            expected[1, 0] = -1; expected[1, 1] = -1;
+
+            Matrix actual = new Matrix(2, 2);
+            actual = a - b;
+
+            Assert.IsTrue(actual == expected);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyException))]
+        public void Matrix_Expected_MyException_Sub()
+        {
+            Matrix a = new Matrix(1, 2);
+            a[0, 0] = 1; a[0, 1] = 1;
+            Matrix b = new Matrix(2, 2);
+            b[0, 0] = 2; b[0, 1] = 2; b[1, 0] = 2; b[1, 1] = 2;
+            Matrix res = a - b;
+        }
+
+        [TestMethod]
+        public void Matrix_Mult()
+        {
+            Matrix a = new Matrix(2, 2);
+            a[0, 0] = 1; a[0, 1] = 1; a[1, 0] = 1; a[1, 1] = 1;
+            Matrix b = new Matrix(2, 2);
+            b[0, 0] = 2; b[0, 1] = 2; b[1, 0] = 2; b[1, 1] = 2;
+
+            Matrix expected = new Matrix(2, 2);
+            expected[0, 0] = 4; expected[0, 1] = 4;
+            expected[1, 0] = 4; expected[1, 1] = 4;
+
+            Matrix actual = new Matrix(2, 2);
+            actual = a * b;
+
+            Assert.IsTrue(actual == expected);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyException))]
+        public void Matrix_Expected_MyException_Mult()
+        {
+            Matrix a = new Matrix(2, 3);
+            a[0, 0] = 1; a[0, 1] = 1; a[0, 2] = 1; a[1, 0] = 1; a[1, 1] = 1; a[1, 2] = 1;
+            Matrix b = new Matrix(2, 3);
+            b[0, 0] = 1; b[0, 1] = 1; b[0, 2] = 1; b[1, 0] = 1; b[1, 1] = 1; b[1, 2] = 1;
+
+            Matrix actual = new Matrix(2, 2);
+            actual = a * b;
+        }
+
+        [TestMethod]
+        public void Matrix_Transp()
+        {
+            Matrix a = new Matrix(2, 2);
+            a[0, 0] = 1; a[0, 1] = 2; a[1, 0] = 3; a[1, 1] = 4;
+
+            Matrix expected = new Matrix(2, 2);
+            expected[0, 0] = 1; expected[0, 1] = 3;
+            expected[1, 0] = 2; expected[1, 1] = 4;
+
+            Matrix actual = new Matrix(2, 2);
+            actual = Matrix.Transp(a);
+
+            Assert.IsTrue(actual == expected);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyException))]
+        public void Matrix_Expected_MyException_Transp()
+        {
+            Matrix a = new Matrix(1, 2);
+            a[0, 0] = 1; a[0, 1] = 2;
+
+            Matrix actual = new Matrix(1, 2);
+            actual = Matrix.Transp(a);
+        }
+
+        [TestMethod]
+        public void Matrix_Min()
+        {
+            Matrix a = new Matrix(2, 2);
+            a[0, 0] = 2; a[0, 1] = 5; a[1, 0] = 1; a[1, 1] = 8;
+
+            int expected = 1;
+
+            int actual = Matrix.Min(a);
+            Assert.IsTrue(actual == expected);
+        }
+
+        [TestMethod]
+        public void Matrix_ToString()
+        {
+            Matrix a = new Matrix(2, 2);
+            a[0, 0] = 2; a[0, 1] = 5; a[1, 0] = 1; a[1, 1] = 8;
+
+            string[,] expected  = new string[2, 2];
+            expected[0, 0] = "2"; expected[0, 1] = "5"; expected[1, 0] = "1"; expected[1, 1] = "8";
+
+            string[,] actual = Matrix.ToString(a);
+            if (expected[0, 0] == actual[0, 0] &&
+                expected[0, 1] == actual[0, 1] &&
+                expected[1, 0] == actual[1, 0] &&
+                expected[1, 1] == actual[1, 1])
+                Assert.IsTrue(1 == 1);
+        }
+    }
+}
